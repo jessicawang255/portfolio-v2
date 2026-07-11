1920ffbadecdf3fdc1836fc752c91b2f4cf8d7dd
jessicawang255 <jessica.wang255@gmail.com>
2026-07-11 01:01:10 -0400

    more spotify connection stuff

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,19 @@
 import { AboutContent } from "@/components/sections/AboutContent"
+import { getTracksByUrls } from "@/lib/spotify"
 
-export default function AboutPage() {
-  return <AboutContent />
+const PLAYLIST_URLS = [
+  "https://open.spotify.com/track/3heQu7YLQFf0WY4Z2giXhk",
+  "https://open.spotify.com/track/6XuQzFKU3rJNNEEs6lyIuQ",
+  "https://open.spotify.com/track/7Dexi5Z2IowCkHrnzlWysc",
+  "https://open.spotify.com/track/5K8lOqPnsWh9QNSgNBqhO2",
+  "https://open.spotify.com/track/6wm3t4VpTxSFfOUgTMlHZM",
+  "https://open.spotify.com/track/5DTqcZlVd7HmXxRAIcWKZo",
+  "https://open.spotify.com/track/5cvFePHQn46kDhgruTPV3M",
+  "https://open.spotify.com/track/15vg0v6tZ1y8aZfpdz2KRY",
+  "https://open.spotify.com/track/5YIVVzQcJG7pFhyNo0Ytlh",
+]
+
+export default async function AboutPage() {
+  const spotifyPlaylist = await getTracksByUrls(PLAYLIST_URLS)
+  return <AboutContent spotifyPlaylist={spotifyPlaylist} />
 }
