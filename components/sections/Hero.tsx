a2b4b62b966367eeca6d5c0c8346d45bce315aaf
jessicawang255 <jessica.wang255@gmail.com>
2026-09-17 12:23:48 -0400

    more tweaks

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -37,7 +37,7 @@ export function Hero() {
 
         <motion.p
           variants={fadeUp}
-          className="mb-2 max-w-xl text-balance font-normal text-neutral-600 text-lg leading-snug"
+          className="mb-3 max-w-xl text-balance font-normal text-neutral-600 text-lg leading-snug"
         >
           I'm a technical product designer who creates experiences that foster community and delight.
         </motion.p>
